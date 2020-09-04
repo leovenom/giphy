054490e7b9c6c6018d0002f18b4e5c05254912e3
leovenom <leonardt@oputlook.com>
2020-09-04 17:58:11 +0100

    gifs showing

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,27 +1,35 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import Loader from "./Loader";
 
 const Giphy = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
     
   useEffect(()=> {
      const fetchData = async () => {
-
+        setIsLoading(true)
       const results = await axios(
         "https://api.giphy.com/v1/gifs/trending", {
           params: {
-            api_key:'w3pDICM0iVgPovQb7sYOjbXvfOHnXX1k'
+            api_key:'w3pDICM0iVgPovQb7sYOjbXvfOHnXX1k',
+            limit: 500
           }
       });
 
       console.log(results)
       setData(results.data.data);
+
+      setIsLoading(false)
      };
  
      fetchData()
   }, []);
 
   const renderGifs = () => {
+      if(isLoading) {
+        return <Loader />;
+      }
     return data.map(el => { 
       return (
         <div key={el.id} className="gif">
---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <i className="fas fa-spinner fa-pulse fa-3x" />
+    </div>
+  );
+};
+
+export default Loader;
